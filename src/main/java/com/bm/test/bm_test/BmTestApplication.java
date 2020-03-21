0bce5b0e878c072f1335392c3c0292beb5966b2c
Gerardo Alias <gerardo.alias@globant.com>
2020-03-21 13:41:45 -0300

    [0001] - Initial commit

---
+++ src/main/java/com/bm/test/bm_test/BmTestApplication.java
@@ -0,0 +1,13 @@
+package com.bm.test.bm_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bm/test/bm_test/BmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bm.test.bm_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
